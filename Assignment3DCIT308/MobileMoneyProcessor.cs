afe595dd03ee4a2a28cc69949d000e3b8976dd9e
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-06 13:55:35 +0000

    Add MobileMoneyProcessor class for transaction processing Introduces the `MobileMoneyProcessor` class in the `Assignment3DCIT308` namespace, implementing the `ITransactionProcessor` interface. The class includes a `Process` method that logs transaction details to the console. Additional `using` directives have been added for necessary namespaces.

---
+++ Assignment3DCIT308/MobileMoneyProcessor.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Assignment3DCIT308
+{
+    internal class MobileMoneyProcessor : ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[Mobile Money]: An amount of {transaction.Amount} has been processed for the {transaction.Category} category");
+        }
+    }
+}
