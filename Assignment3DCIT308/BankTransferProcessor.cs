8acad29af6e9d0cb3edda13b01cf671444086f07
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-06 13:46:33 +0000

    Add BankTransferProcessor class for transaction processing
    Introduces the `BankTransferProcessor` class in the `Assignment3DCIT308` namespace, implementing the `ITransactionProcessor` interface. The class includes a `Process` method that logs transaction details to the console. Additional `using` directives have been added for necessary namespaces.

---
+++ Assignment3DCIT308/BankTransferProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3DCIT308
+{
+    internal class BankTransferProcessor : ITransactionProcessor
+    {
+       public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[Bank Transfer]: An amount of {transaction.Amount} has been processed for the {transaction.Category} category");
+        }
+
+
+    }
+}
+
