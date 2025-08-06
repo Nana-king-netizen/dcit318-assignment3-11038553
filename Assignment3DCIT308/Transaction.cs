6164a63de387cbf0671ec48bb0415ccb5a54da55
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-06 13:45:33 +0000

    Add Transaction record in Assignment3DCIT308 namespace
    Introduced a new namespace `Assignment3DCIT308` that contains an internal record `Transaction`. The record includes properties for `Id`, `Date`, `Amount`, and `Category`, facilitating better data management for transactions.

---
+++ Assignment3DCIT308/Transaction.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Assignment3DCIT308
+{
+    internal record Transaction
+    {
+        public int Id;
+       public  DateTime Date;
+       public decimal Amount;
+        public string Category;
+    }
+}
