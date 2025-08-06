8f624e7039c645f09141aaa54b0e378404454cba
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-06 13:45:59 +0000

    Add Account class with balance management
    Introduces a new `Account` class in the `Assignment3DCIT308` namespace. The class includes a private `AccountNumber` property and a public `Balance` property. It features a constructor for initialization and a virtual method `ApplyTransaction` to deduct transaction amounts from the balance.

---
+++ Assignment3DCIT308/Account.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Assignment3DCIT308
+{
+    internal class Account
+    {
+        string AccountNumber { get; set; }
+        public decimal Balance { get; protected set; }
+
+        public Account(string accountNumber, decimal initialBalance)
+        {
+            this.AccountNumber = accountNumber;
+            this.Balance = initialBalance;
+        }
+
+        public virtual void ApplyTransaction(Transaction transaction)
+        {
+            Balance -= transaction.Amount;
+        }
+    }
+}
