08d90e695805a6664a5fbaed8a185f165450db4f
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-06 13:47:02 +0000

    Add CryptoWalletProcessor for transaction processing
    Introduced a new namespace `Assignment3DCIT308` with an internal class `CryptoWalletProcessor` that implements the `ITransactionProcessor` interface. The `Process` method outputs transaction details to the console, enhancing transaction handling capabilities.

---
+++ Assignment3DCIT308/CryptoWalletProcessor.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Assignment3DCIT308
+{
+    internal class CryptoWalletProcessor:ITransactionProcessor
+    {
+        public void Process(Transaction transaction)
+        {
+            Console.WriteLine($"[Crypto Wallet]: An amount of {transaction.Amount} has been processed for the {transaction.Category} category");
+        }
+    }
+}
