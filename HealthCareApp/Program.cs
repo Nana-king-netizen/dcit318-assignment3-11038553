a9963f958c8ab07486395127e655e1af08a897d5
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 13:49:32 +0000

    Add Program.Main to run healthcare system app

---
+++ HealthCareApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using HealthCareApp;
+
+
+namespace HealthcareApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new HealthSystemApp();
+            app.SeedData();
+            app.BuildPrescriptionMap();
+            app.PrintAllPatients();
+
+            Console.Write("\nEnter Patient ID to view prescriptions: ");
+            if (int.TryParse(Console.ReadLine(), out int patientId))
+            {
+                app.PrintPrescriptionsForPatient(patientId);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
