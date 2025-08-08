9122a04e29b227dcab26c75cb46361a430f51a7b
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 18:36:43 +0000

    Implement generic InventoryRepository with CRUD operations and validations

---
+++ Warehouse Inventory/InventoryRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Inventory
+{
+    internal class InventoryRepository<T> where T : IInventoryItem
+    {
+        Dictionary<int, T> _items = new Dictionary<int, T>();
+
+        public void AddItem(T item)
+        {
+            if (_items.ContainsKey(item.Id))
+            {
+                throw new DuplicateItemException($"Item with ID{item.Id} already exists");
+            }
+            else
+            {
+                _items[item.Id] = item;
+            }
+        }
+
+        public T GetItemById(int id)
+        {
+            if (!_items.ContainsKey(id))
+            {
+                throw new ItemNotFoundException($"Item with ID {id} not found");
+            }
+            else
+            {
+                return _items[id];
+            }
+        }
+
+        public void RemoveItem(int id)
+        {
+            if (!_items.ContainsKey(id))
+            {
+                throw new ItemNotFoundException($"Item with ID {id} not found");
+            }
+            else
+            {
+                _items.Remove(id);
+            }
+
+        }
+
+        public List<T> GetAllItems()
+        {
+            return new List<T>(_items.Values);
+        }
+
+        public void UpdateQuantity(int id, int newQuantity)
+        {
+            if (newQuantity < 0)
+                throw new InvalidQuantityException("Quantity cannot be negative.");
+            if (!_items.ContainsKey(id))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+            _items[id].Quantity = newQuantity;
+        }
+    }
+}
