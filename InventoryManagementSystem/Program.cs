81ba19f9aa196e1aeff2663b070d18ad9077cfb8
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 20:46:51 +0000

    Add main program flow to save and reload inventory data from file

---
+++ InventoryManagementSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagementSystem;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "inventory.json";
+
+   
+        var app = new InventoryApp(filePath);
+        app.SeedSampleData();
+        app.SaveData();
+
+        Console.WriteLine("Data saved.");
+
+   
+        var newApp = new InventoryApp(filePath);
+        newApp.LoadData();
+        newApp.PrintAllItems();
+    }
+}
+
