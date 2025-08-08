6ff780a6adb1f388bcede4997283275dbb9e9e28
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 18:39:50 +0000

    Add IInventoryItem interface for inventory items

---
+++ Warehouse Inventory/IInventoryItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Inventory
+{
+    internal interface IInventoryItem
+    {
+       public int Id { get; }
+       public string Name { get; }
+     public int Quantity { get; set; }
+    }
+}
