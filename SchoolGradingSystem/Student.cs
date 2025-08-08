2b3c963f5d7f850f04aa370565c02a88ed2d4423
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 20:20:10 +0000

    Add Student class with fields, constructor, and GetGrade method

---
+++ SchoolGradingSystem/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGradingSystem
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Score { get; set; }
+
+        public string GetGrade()
+        {
+            if (Score >= 80 && Score <= 100) return "A";
+            if (Score >= 70 && Score <= 79) return "B";
+            if (Score >= 60 && Score <= 69) return "C";
+            if (Score >= 50 && Score <= 59) return "D";
+            return "F";
+        }
+    }
+}
