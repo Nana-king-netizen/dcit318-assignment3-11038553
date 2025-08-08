d4e27707ea871ef8a3bb3353651512da47bca2aa
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 20:51:20 +0000

    Implement InventoryApp with sample data seeding, persistence, and display

---
+++ InventoryManagementSystem/InventoryApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    internal class InventoryApp
+    {
+        private InventoryLogger<InventoryItem> _logger;
+
+        public InventoryApp(string filePath)
+        {
+            _logger = new InventoryLogger<InventoryItem>(filePath);
+        }
+
+        public void SeedSampleData()
+        {
+            _logger.Add(new InventoryItem(1, "Laptop", 5, DateTime.Now));
+            _logger.Add(new InventoryItem(2, "Printer", 2, DateTime.Now));
+            _logger.Add(new InventoryItem(3, "Projector", 1, DateTime.Now));
+        }
+
+        public void SaveData() => _logger.SaveToFile();
+
+        public void LoadData() => _logger.LoadFromFile();
+
+        public void PrintAllItems()
+        {
+            foreach (var item in _logger.GetAll())
+            {
+                Console.WriteLine($"ID: {item.Id}, Name: {item.Name}, Quantity: {item.Quantity}, Date Added: {item.DateAdded}");
+            }
+        }
+    }
+}
