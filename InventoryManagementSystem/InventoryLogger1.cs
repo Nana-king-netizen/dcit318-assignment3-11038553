fc9d048fbf9592089a3aecd00d817cd5e00b6fca
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 20:49:36 +0000

    Create generic InventoryLogger with add, retrieve, save, and load methods

---
+++ InventoryManagementSystem/InventoryLogger1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text.Json;
+
+   internal class InventoryLogger<T> where T : IInventoryEntity
+    {
+        private List<T> _log = new List<T>();
+        private string _filePath;
+
+        public InventoryLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Add(T item) => _log.Add(item);
+
+        public List<T> GetAll() => _log;
+
+        public void SaveToFile()
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(_log, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(_filePath, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving file: " + ex.Message);
+            }
+        }
+
+        public void LoadFromFile()
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                    throw new FileNotFoundException("File not found: " + _filePath);
+
+                string json = File.ReadAllText(_filePath);
+                _log = JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading file: " + ex.Message);
+            }
+        }
+    }
+
+}
