990f6e90485dee133e8574c47370b7857cf20b51
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 18:38:29 +0000

    Add ElectronicItem and GroceryItem classes implementing IInventoryItem

---
+++ Warehouse Inventory/ElectronicItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Inventory
+{
+    internal class ElectronicItem:IInventoryItem
+    {
+       
+            public int Id { get; }
+            public string Name { get; }
+            public int Quantity { get; set; }
+            public string Brand { get; }
+            public int WarrantyMonths { get; }
+
+            public ElectronicItem(int id, string name, int quantity, string brand, int warrantyMonths)
+            {
+                this.Id = id;
+                this.Name = name;
+               this.Quantity = quantity;
+                this.Brand = brand;
+               this.WarrantyMonths = warrantyMonths;
+            }
+        }
+    }
---
+++ Warehouse Inventory/GroceryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Inventory
+{
+    internal class GroceryItem:IInventoryItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; }
+
+        public GroceryItem(int id, string name, int quantity, DateTime expiryDate)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Quantity = quantity;
+            this.ExpiryDate = expiryDate;
+        }
+    }
+}
