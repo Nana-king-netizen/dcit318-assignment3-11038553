f0e1dedfc671dc09df320187f7d3c4615c915261
Nana-king-netizen <otengnanayaw354@gmail.com>
2025-08-08 13:47:02 +0000

    Add Patient and Prescription model classes

---
+++ HealthCareApp/Patient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareApp
+{
+    internal class Patient
+    {
+         public int Id { get; set; }
+        public string Name {  get; set; }
+         int Age {  get; set; }
+        string Gender {  get; set; }
+
+        public Patient(int id, string name, int age, string gender)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Age: {Age}, Gender: {Gender}";
+        }
+
+    }
+}
---
+++ HealthCareApp/Prescription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareApp
+{
+    internal class Prescription
+    {
+        int Id {  get; set; }
+        public int PatientId {  get; set; }
+         string MedicationName {  get; set; }
+        DateTime DateIssued {  get; set; }
+
+        public Prescription(int id, int patientId, string medicationName, DateTime dateIssued)
+        {
+            this.Id = id;
+            this.PatientId = patientId;
+            this.MedicationName = medicationName;
+            this.DateIssued = dateIssued;
+        }
+
+        public override string ToString()
+        {
+            return $"Prescription ID: {Id}, Medication: {MedicationName}, Date: {DateIssued:yyyy-MM-dd}";
+        }
+    }
+}
